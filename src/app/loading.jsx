31e0048b520a9cfbe6ebd2cf1d3f30f5ea30434f
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-09-22 18:17:51 +0300

    loading page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,9 +4,7 @@ import React from "react";
 function Loading() {
   return (
     <div className="text-3xl font-extrabold flex items-center justify-center">
-      <h1 className="text-3xl font-extrabold flex items-center justify-center text-slate-950">
-        WelCome to Brain Bird. Your Social Environment.
-      </h1>
+      <h1 className=" text-slate-950">WelCome to Brain Bird. Your Number OneSocial Environment.</h1>
     </div>
   );
 }
