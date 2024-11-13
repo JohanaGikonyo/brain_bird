6c98d6ec4b697c8cb36a57674217dc877bde0843
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-11-13 17:06:51 +0300

    skeleton

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
-
+import Skeleton from './components/Skeleton'
 function Loading() {
   return (
     <div className="text-3xl font-extrabold flex items-center justify-center">
-      <h1 className=" text-slate-950">WelCome to Brain Bird. Your Number OneSocial Environment.</h1>
+      <h1 className=" text-slate-100">WelCome to Brain Bird. Your Number OneSocial Environment.</h1>
+      <Skeleton/>
     </div>
   );
 }
