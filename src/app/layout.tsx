033b1fbe29bd20545cd1f290f1823bc21280630f
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-11-13 12:11:28 +0300

    pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" sizes="any" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" /> 
+        <meta name="theme-color" content="#000000" />
         {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" /> */}
